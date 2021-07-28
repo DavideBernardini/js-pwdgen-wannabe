028aa8adf67cbc3a677b4c1cba413d8fc66adef1
DavideBernardini <davidebernardini09@gmail.com>
2021-07-28 17:31:39 +0200

    completate variabili

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 // nome utente
 var firstname = prompt('inserisci il tuo nome');
+// cognome utente
 var surname = prompt('inserisci il tuo cognome');
+//colore preferito
 var color = prompt('inserisci il tuo colore preferito');
+// creazione password
 var password = firstname + surname + color;
+// Aggiunta password al documento
+document.getElementById('yourpassword').innerHTML += password + 40;
